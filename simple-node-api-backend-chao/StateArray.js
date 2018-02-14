d7394af214ff3eaad13c106a5c637f43ea3a309c
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-14 11:29:40 -0800

    Incorporate twitter api calls

--- simple-node-api-backend-chao/StateArray.js
+++ simple-node-api-backend-chao/StateArray.js
@@ -1,3 +1,14 @@
 [
   NaN, Alabama, Alaska, NaN, Arizona, Arkansas, California, NaN, Colorado, Connecticut, Delaware, NaN, Florida, Georgia, NaN, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, NaN, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, NaN, Washington, West Virginia, Wisconsin, Wyoming
 ]
+
+
+//original state
+var valueById = [
+   NaN, .187, .198,  NaN, .133, .175, .151,  NaN, .100, .125,
+  .171,  NaN, .172, .133,  NaN, .108, .142, .167, .201, .175,
+  .159, .169, .177, .141, .163, .117, .182, .153, .195, .189,
+  .134, .163, .133, .151, .145, .130, .139, .169, .164, .175,
+  .135, .152, .169,  NaN, .132, .167, .139, .184, .159, .140,
+  .146, .157,  NaN, .139, .183, .160, .143
+];
---
+++ simple-node-api-backend-chao/twitter.js
@@ -0,0 +1,15 @@
+const _twitter = require('twitter');
+
+var twitter = new _twitter({
+  "consumer_key": 'XHejtyytxyQpXx2wI0dOw08eH',
+  "consumer_secret": 'aoqSPa32XmPey8dVCTqtWRNjmaiEmFPAqYghGFY7ZcAXN6fnkR',
+  "access_token_key": '963316414021906433-TFseaRObb3EMyVBEXMxvsZ69f3YAwtK',
+  "access_token_secret": 'KWk1xAVNW4b02PdxJvAXOgwuVzmUTGdlkUZM4QRXkobrO',
+});
+
+
+twitter.get('search/tweets.json', {q: 'node.js'}, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
