d9827c0b8dd899d5855551a9f1c7054165c19489
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-13 17:32:43 -0800

    Add non-contiguous cartogram

---
+++ simple-node-api-backend-chao/StateArray.js
@@ -0,0 +1,3 @@
+[
+  NaN, Alabama, Alaska, NaN, Arizona, Arkansas, California, NaN, Colorado, Connecticut, Delaware, NaN, Florida, Georgia, NaN, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, NaN, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, NaN, Washington, West Virginia, Wisconsin, Wyoming
+]
